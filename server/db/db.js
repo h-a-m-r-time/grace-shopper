e36c899aa4ed3ce471ed7a8083eda027c723ba46
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-04 16:15:15 -0400

    more updates around renaming

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+console.log("TESTING LOG", process.env.NODE_ENV, pkg.name)
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
