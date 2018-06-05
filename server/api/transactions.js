831d4735e4d3328cfb9c986868acc180d698f004
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-05 16:58:01 -0400

    initial transactions

---
+++ server/api/transactions.js
@@ -0,0 +1,48 @@
+const router = require('express').Router()
+const {Transaction} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+    try {
+        const transactions = await Transaction.findAll({where: {purchased: false}})
+        res.json(transactions)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const transaction = await Transaction.findById(req.params.id)
+        res.json(transaction)
+    } catch (error){
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const transaction = await Transaction.create(req.body)
+        res.json(transaction)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+      const transaction = await Transaction.update(req.body, { where: {id: req.body.id} })
+      res.json(transaction)
+  } catch (err){
+      next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+      Transaction.destroy({where: {id: +req.params.id}})
+      res.json(req.params.id)
+  } catch (err){
+      next(err)
+  }
+})
