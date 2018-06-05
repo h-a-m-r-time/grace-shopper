282277d3ffbc85104afe32bbc403a0c7421462c4
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-05 16:13:05 -0400

    unique reinstated on user email definition

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -5,7 +5,7 @@ const db = require('../db')
 const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
-    // unique: true,
+    unique: true,
     allowNull: false
   },
   password: {
