ff078fb5df76cfe274aa0a885b1bd6185857f41e
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-06 17:36:45 -0400

    added thing api route tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -6,6 +6,7 @@ const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
 const Feeling = db.model('feeling')
+const Thing = db.model('thing')
 
 describe('User routes', () => {
   beforeEach(() => {
@@ -78,6 +79,56 @@ describe('Feeling routes', () => {
         .then(res => {
           expect(res.body).to.be.an('object')
           expect(res.body.name).to.be.equal( secondFeeling )
+        })
+    })
+  })
+})
+
+describe('Thing routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('api/things/', () => {
+    const firstThing = 'apples'
+    const secondThing = 'oranges'
+    let apples
+
+    beforeEach(() => {
+
+      const things = [{
+        name: firstThing,
+        description: 'one a day keeps the doctor away'
+      }, {
+        name: secondThing,
+        description: 'at least it\'s not a banana'
+      }]
+
+
+      return Thing.bulkCreate(things, {returning: true})
+        .then(createdThings => {
+          apples = createdThings[0].id
+        })
+    })
+
+    it('GET /api/things', () => {
+      return request(app)
+        .get('/api/things')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[1].name).to.be.equal(secondThing)
+          expect(res.body[0].description).to.be.equal('one a day keeps the doctor away')
+        })
+    })
+
+    it('serves up a specific feeling on GET /things/{id}', () => {
+      return request(app)
+        .get(`/api/things/${apples}`)
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('object')
+          expect(res.body.name).to.be.equal( firstThing )
         })
     })
   })
