40bf251e9621ba6bee25386b96bbca271d81cc83
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-06 16:50:40 -0400

    created feeling api route tests

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
+const Feeling = db.model('feeling')
 
 describe('User routes', () => {
   beforeEach(() => {
@@ -31,3 +32,46 @@ describe('User routes', () => {
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
+
+describe('Feeling routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('api/feelings/', () => {
+    const firstFeeling = 'hate'
+    const secondFeeling = 'love'
+    let love
+
+    beforeEach(() => {
+      return Feeling.bulkCreate([{
+        name: firstFeeling
+      }, {
+        name: secondFeeling
+      }])
+        .then(createdFeelings => {
+          love = createdFeelings[1].id
+        })
+    })
+
+    it('GET /api/feelings', () => {
+      return request(app)
+        .get('/api/feelings')
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('array')
+          expect(res.body[0].name).to.be.equal(firstFeeling)
+        })
+    })
+
+    it('serves up a specific feeling on GET /feelings/{id}', () => {
+      return request(app)
+        .get(`/feelings/${love}`)
+        .expect(200)
+        .then(res => {
+          expect(res.body).to.be.an('object')
+          expect(res.body).to.be.equal({ name: secondFeeling })
+        })
+    })
+  })
+})
