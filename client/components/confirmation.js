13715a50926d44f20cdcd807dddd268d7b0c88c5
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-07 21:31:57 -0400

    table can't have text node child bug fix

--- client/components/confirmation.js
+++ client/components/confirmation.js
@@ -25,7 +25,7 @@ class Confirmation extends Component {
           }
       })
     } else {
-      return 'We can not find your transactions, the pyramid is collapsing'
+      return <TableRow>'We can not find your transactions, the pyramid is collapsing'</TableRow>
     }
   }
 
