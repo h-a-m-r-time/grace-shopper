f9397e965ee4db4b5fea78abc0c0c260777acf68
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-11 17:49:15 -0400

    fix: opinions eager loading

--- server/api/opinions.js
+++ server/api/opinions.js
@@ -5,10 +5,16 @@ module.exports = router
 router.get('/', async (req, res, next) => {
     try {
         const opinions = await Opinion.findAll({
-            where: {purchased: true},
             include: [{all: true}]
         })
-        res.json(opinions)
+        const purchased = opinions.map(op => {
+            if(opinions.transactions.legnth > 0){
+                opinions.transactions.filter(trns => {
+                    return trns.purchased
+                })
+            }
+        })
+        res.json(purchased)
     } catch (error) {
         next(error)
     }
