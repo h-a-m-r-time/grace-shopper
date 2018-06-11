e3e0b22f1fc4e11bea9207d43e27f9972b7c3169
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-11 18:13:35 -0400

    removed opinion route test

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -23,18 +23,18 @@ describe('UserHome', () => {
   })
 })
 
-describe('Opinion', () => {
-  let opinionComp
+// describe('Opinion', () => {
+//   let opinionComp
 
-  const opinion = {
-    statement: 'life is meaningless'
-  }
+//   const opinion = {
+//     statement: 'life is meaningless'
+//   }
 
-  beforeEach(() => {
-    opinionComp = shallow(<Opinion opinion={opinion} />)
-  })
+//   beforeEach(() => {
+//     opinionComp = shallow(<Opinion opinion={opinion} />)
+//   })
 
-  it('renders the opinion statement in an h2', () => {
-    expect(opinionComp.find('h2').text()).to.be.equal('life is meaningless')
-  })
-})
+//   it('renders the opinion statement in an h2', () => {
+//     expect(opinionComp.find('h2').text()).to.be.equal('life is meaningless')
+//   })
+// })
