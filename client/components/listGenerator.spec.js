003f0005863592b3d91883b828767b07daefd3bd
Huu Nguyen <huu.nguyen@gmail.com>
2018-06-12 15:28:37 -0400

    feat: added all opinion button test: added list Generator React test

--- client/components/listGenerator.spec.js
+++ client/components/listGenerator.spec.js
@@ -2,9 +2,13 @@
 
 import { expect } from 'chai'
 import React from 'react'
-import enzyme, { shallow } from 'enzyme'
+import enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import ListGenerator from './listGenerator'
+const { JSDOM } = require('jsdom')
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+const { window } = jsdom
 
 // import Opinion from './opinion'
 
@@ -13,15 +17,14 @@ enzyme.configure({ adapter })
 
 describe('ListGenerator React Component', () => {
   let listComp
+  const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
+  const { window } = jsdom
 
   beforeEach(() => {
-    listComp = shallow(<ListGenerator />)
+    listComp = shallow(<ListGenerator />).dive({ context })
   })
 
   it('renders default state = myOpinions', () => {
-    console.log(listComp.state())
-    expect(listComp.state()).to.be.deep.equal({
-      displayOrder: 'myOpinions',
-    })
+    expect(listComp.state('displayOrder')).to.be.deep.equal('allOpinions')
   })
 })
