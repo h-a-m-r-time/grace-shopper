7df61942e123d6d655f2a24c0058a2d73e5f93a9
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-11 20:39:48 -0400

    document import of Opinion removed

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -6,7 +6,7 @@ import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {UserHome} from './user-home'
 
-import Opinion from './opinion'
+// import Opinion from './opinion'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
@@ -22,3 +22,19 @@ describe('UserHome', () => {
     expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
   })
 })
+
+// describe('Opinion', () => {
+//   let opinionComp
+
+//   const opinion = {
+//     statement: 'life is meaningless'
+//   }
+
+//   beforeEach(() => {
+//     opinionComp = shallow(<Opinion opinion={opinion} />)
+//   })
+
+//   it('renders the opinion statement in an h2', () => {
+//     expect(opinionComp.find('h2').text()).to.be.equal('life is meaningless')
+//   })
+// })
