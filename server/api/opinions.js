bfd9438895ac11e378d81eaaf8cc55a961b05f5e
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-12 11:33:43 -0400

    feat: browser for opinions and only get ones that have purchased transactions

--- server/api/opinions.js
+++ server/api/opinions.js
@@ -1,34 +1,13 @@
 const router = require('express').Router()
-const {Opinion} = require('../db/models')
+const {Opinion, Transaction} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
     try {
         const opinions = await Opinion.findAll({
-            include: [{all: true}]
+            include: [{model: Transaction, where: {purchased: true}}]
         })
-        //trying to filter out opinions' transactions that flagged purchased as false
-        //everything returns as expected but then when the res is sent it ignores teh purchased property
-        //originally i was trying to jsut modify the transactions property but still wouldn't change length
-        //even though it was reporting it did change
-        //I suspect maybe this is something about persisting a sequelize return object?
-        const purchased = opinions.map(op => {
-            console.log("IN HERE THOUGH!!!!!!!!!!!!!!!!!!!!!", op.transactions)
-            let transactions;
-            if(op.transactions && op.transactions.length > 0){
-                console.log("IN HERE", op.transactions.length)
-                transactions = op.transactions.filter(trns => {
-                    console.log(trns.purchased)
-                    return trns.purchased === true
-                })
-            }
-            console.log("OUT HERE", op.transactions.length, transactions.length)
-            op.purchased = transactions
-            console.log(op.purchased, op)
-            return op
-        })
-        console.log("OUT MORE", purchased[2].transactions.length, purchased[2].purchased.length)
-        res.json(purchased)
+        res.json(opinions)
     } catch (error) {
         next(error)
     }
