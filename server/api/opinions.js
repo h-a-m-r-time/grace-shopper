4a9f00cef16eeac7298350a7c7dfa1a6dd2bced3
Andrew Clavin <andrewclavin8@gmail.com>
2018-06-11 21:37:52 -0400

    feat-wip: trying to get opinions to return a filtered subset of transactions for purchases

--- server/api/opinions.js
+++ server/api/opinions.js
@@ -7,17 +7,27 @@ router.get('/', async (req, res, next) => {
         const opinions = await Opinion.findAll({
             include: [{all: true}]
         })
+        //trying to filter out opinions' transactions that flagged purchased as false
+        //everything returns as expected but then when the res is sent it ignores teh purchased property
+        //originally i was trying to jsut modify the transactions property but still wouldn't change length
+        //even though it was reporting it did change
+        //I suspect maybe this is something about persisting a sequelize return object?
         const purchased = opinions.map(op => {
             console.log("IN HERE THOUGH!!!!!!!!!!!!!!!!!!!!!", op.transactions)
+            let transactions;
             if(op.transactions && op.transactions.length > 0){
-                console.log("IN HERE")
-                op.transactions = op.transactions.filter(trns => {
+                console.log("IN HERE", op.transactions.length)
+                transactions = op.transactions.filter(trns => {
                     console.log(trns.purchased)
-                    return trns.purchased
+                    return trns.purchased === true
                 })
             }
+            console.log("OUT HERE", op.transactions.length, transactions.length)
+            op.purchased = transactions
+            console.log(op.purchased, op)
             return op
         })
+        console.log("OUT MORE", purchased[2].transactions.length, purchased[2].purchased.length)
         res.json(purchased)
     } catch (error) {
         next(error)
